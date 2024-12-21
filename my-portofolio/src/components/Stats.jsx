670a1d45a499de351cf6ea26c3a03b59703796a5
FatlumG <167577293+FatlumG@users.noreply.github.com>
2024-12-21 17:56:50 +0100

    Made stats section responsive

--- my-portofolio/src/components/Stats.jsx
+++ my-portofolio/src/components/Stats.jsx
@@ -3,7 +3,10 @@ import "../styles/components/stats.css";
 
 function Stats() {
   return (
-    <div id="about" className="stats d-flex justify-content-center align-items-center gap-3 text-white">
+    <div
+      id="about"
+      className="stats d-flex flex-column flex-md-row justify-content-center align-items-center gap-3 text-white px-5 py-4 py-md-0"
+    >
       <div className="statistic d-flex flex-column justify-content-center align-items-center">
         <h2 className="fs-1 fw-bold">4.5</h2>
         <p>Stars Rating</p>
